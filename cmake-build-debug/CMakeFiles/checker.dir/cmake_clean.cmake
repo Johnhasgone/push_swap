ee27fbb4e0e20977979d69f28b96062a512b98f3
johnhasgone <89quAdrA89>
2019-12-04 22:04:58 +0300

    trying the second variant

--- cmake-build-debug/CMakeFiles/checker.dir/cmake_clean.cmake
+++ cmake-build-debug/CMakeFiles/checker.dir/cmake_clean.cmake
@@ -1,11 +1,11 @@
 file(REMOVE_RECURSE
+  "CMakeFiles/checker.dir/checker.c.o"
+  "CMakeFiles/checker.dir/checker_main.c.o"
   "CMakeFiles/checker.dir/instructions.c.o"
   "CMakeFiles/checker.dir/instructions_2.c.o"
-  "CMakeFiles/checker.dir/checker.c.o"
   "CMakeFiles/checker.dir/push_swap.c.o"
-  "CMakeFiles/checker.dir/checker_main.c.o"
-  "checker.pdb"
   "checker"
+  "checker.pdb"
 )
 
 # Per-language clean rules from dependency scanning.
